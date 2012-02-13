8711f9d6d311d0c12702c30138cc20ca121af664
Hideyuki TAKEUCHI <chimerast@gmail.com>
2012-02-13 07:43:48 +0000

    微調整

--- src/main/java/root/dao/CorpBssDao.java
+++ src/main/java/root/dao/CorpBssDao.java
@@ -2,6 +2,7 @@ package root.dao;
 
 import java.util.List;
 
+import org.seasar.dao.annotation.tiger.Query;
 import org.seasar.dao.annotation.tiger.S2Dao;
 import org.seasar.dao.annotation.tiger.Sql;
 import org.seasar.dao.pager.PagerCondition;
@@ -11,9 +12,8 @@ import root.entity.CorpBss;
 @S2Dao(bean = CorpBss.class)
 public interface CorpBssDao {
 
+    @Query("ORDER BY global_id")
     public List<CorpBss> selectPage(PagerCondition condition);
-
-    public List<CorpBss> selectAll();
 
     @Sql("SELECT count(*) FROM corp_bss")
     public int count();
--- src/main/java/root/web/IndexPage.java
+++ src/main/java/root/web/IndexPage.java
@@ -2,11 +2,11 @@ package root.web;
 
 import java.util.Iterator;
 
-import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.seasar.dao.pager.DefaultPagerCondition;
@@ -23,39 +23,40 @@ public class IndexPage extends WebBasePage {
     private CorpBssDao corpBssDao;
 
     public IndexPage() {
-        final SortableDataProvider<CorpBss> provider =
-                new SortableDataProvider<CorpBss>() {
-                    private static final long serialVersionUID = 1L;
+        final IDataProvider<CorpBss> provider = new IDataProvider<CorpBss>() {
+            private static final long serialVersionUID = 1L;
 
-                    @Override
-                    public int size() {
-                        return corpBssDao.count();
-                    }
+            @Override
+            public void detach() {
+            }
 
-                    @Override
-                    public IModel<CorpBss> model(CorpBss object) {
-                        return new CompoundPropertyModel<CorpBss>(object);
-                    }
+            @Override
+            public int size() {
+                return corpBssDao.count();
+            }
 
-                    @Override
-                    public Iterator<? extends CorpBss> iterator(int first,
-                            int count) {
-                        PagerCondition condition = new DefaultPagerCondition();;
-                        condition.setOffset(first);
-                        condition.setLimit(count);
-                        return corpBssDao.selectPage(condition).iterator();
-                    }
-                };
+            @Override
+            public IModel<CorpBss> model(CorpBss object) {
+                return new CompoundPropertyModel<CorpBss>(object);
+            }
+
+            @Override
+            public Iterator<? extends CorpBss> iterator(int first, int count) {
+                PagerCondition condition = new DefaultPagerCondition();;
+                condition.setOffset(first);
+                condition.setLimit(count);
+                return corpBssDao.selectPage(condition).iterator();
+            }
+        };
 
         final DataView<CorpBss> companies;
-        add(companies = new DataView<CorpBss>("companies", provider, 10) {
+        add(companies = new DataView<CorpBss>("companies", provider, 20) {
             private static final long serialVersionUID = 1L;
 
             @Override
             protected void populateItem(Item<CorpBss> item) {
-                CorpBss corpBss = item.getModelObject();
-                item.add(new Label("id", corpBss.globalId));
-                item.add(new Label("name", corpBss.corpFrmlNameVch));
+                item.add(new Label("globalId"));
+                item.add(new Label("corpFrmlNameVch"));
             }
         });
         add(new PagingNavigator("navigator", companies));
